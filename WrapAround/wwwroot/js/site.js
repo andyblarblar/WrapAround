e1f0cbbf6ff5a9c9649d9394076786fd0e85b522
ajgouge481 <gougeonaj@gmail.com>
2020-02-19 13:01:56 -0500

    started frontend ecks dee

--- WrapAround/wwwroot/js/site.js
+++ WrapAround/wwwroot/js/site.js
@@ -1,5 +1,27 @@
-﻿
-//function addPlayerToLobby() {
-//    connection.invoke("AddPlayer");
-//}
+﻿"use strict";
+const scn = document.getElementById("render-window");
+const lobs = [
+    document.getElementById("lobby-1"),
+    document.getElementById("lobby-2"),
+    document.getElementById("lobby-3"),
+    document.getElementById("lobby-4")
+];
 
+const _lobbyCounts = [];
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/game")
+    .withAutomaticReconnect()
+    .configureLogging(signalR.LogLevel.Trace)
+    .build();
+
+connection.start().then(function () {
+    console.log("connected");
+})
+
+connection.invoke("GetLobbyPlayerCounts");
+
+connection.on("ReceiveLobbyCounts", (lobbyCounts) => {
+    _lobbyCounts = lobbyCounts;
+    //update lobby boxes
+});
